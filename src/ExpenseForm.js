e9adfc8e16748394cde9503903414e22e665ee79
vijay <vijay951mane@gmail.com>
2022-01-28 22:50:35 +0530

    commit;

--- src/ExpenseForm.js
+++ src/ExpenseForm.js
@@ -1,27 +1,57 @@
+import {useState} from "react";
 const ExpenseForm = () =>{
+    let [expName, setExpName] = useState("N");
+    console.log(expName);
+    function expNameHandler(e){
+        // console.log(e.target.value);
+        // expName =  e.target.value
+        // console.log(`ExpName val:${expName}`);
+        // OR
+        // console.log('ExpName val:', expName)
+        setExpName(e.target.value);
+        setTimeout(()=> console.log(expName),5000);
+        console.log(`ExpName val: ${expName}`);
+    }
+
+    let [expAmt, setExpAmt] = useState("Amount");
+    console.log(expAmt);
+    function expAmountHandler(e){
+        // console.log(e.target.value);
+        // expName =  e.target.value
+        // console.log(`ExpAmt val:${expAmt}`);
+        // OR
+        // console.log('ExpName val:', expName)
+        setExpAmt(e.target.value);
+        console.log(`ExpAmount val: ${expAmt}`);
+    }
+    
+    let [expDate, setExpDate] = useState("Date");
+    console.log(expDate)
+    function expDateHandler(e){
+        setExpDate(e.target.value);
+        console.log(`ExpDate val: ${expDate}`);
+    }
+
     return(
-        // <form>
-        //     <div>
-        //         <label htmlFor="expName">
-        //             <input type={}>
-        //             </input>
-        //         </label>
-        //     </div>
-        // </form>
-        <form>
+        <form id="one">
             <div>
                 <label htmlFor="expName">Expense Name: </label>
-                <input type ="text" id="expName" placeholder="Enter your Expense Name"></input>
-            </div>
+                <input type ="text" id="expName" placeholder="Enter your Expense Name" onInput={expNameHandler}></input>
+            </div><br></br><br></br>
             <div>
-                <label htmlFor="expAmt">Expense Amount: </label>
-                <input type="number" id="expAmt" placeholder="Enter your Expense Amt"></input>
-            </div>
+                <label htmlFor="expAmt">Expense Amount:</label>
+                <input type="number" id="expAmt" placeholder="Enter your Expense Amount" onChange={expAmountHandler}></input>
+            </div><br></br><br></br>
             <div>
-                <label htmlFor = "expDate"> Expense Date: </label> 
-                <input type="date" id="expDate"></input>
+                <label htmlFor= "expDate">Expense Date:</label> 
+                <input type="date" id="expDate" onChange={expDateHandler}></input>
+            </div><br></br><br></br>
+            <div>
+                <center><button type="submit"> Add Expense</button></center>
             </div>
-            <button type="submit"> Add Expense</button>
+            <p>{expName}</p>
+            <p>{expAmt}</p>
+            <p>{expDate}</p> 
         </form>
 
     )
