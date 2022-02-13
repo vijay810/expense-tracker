379e4b3c3c82e3bd4375aaa6d3804a9639ecd552
vijay <vijay951mane@gmail.com>
2022-02-13 17:49:08 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import {useEffect, useState} from "react";
-import ExpenseForm from './ExpenseForm';
-import ExpenseView from './ExpenseView';
-
+import { useEffect, useState } from "react";
+import ExpenseForm from "./ExpenseForm";
+import ExpenseView from "./ExpenseView";
 
 function App() {
-  const[expData, setExpData]=useState([])
+  const [expData, setExpData] = useState([]);
 
-  async function getData(){
+  async function getData() {
     const resp = await fetch(`http://localhost:3002/`);
     const respObj = await resp.json();
     console.log(respObj);
@@ -14,44 +13,73 @@ function App() {
   }
   //useEffect
   //it is used to hansdle use effect
-  useEffect( () =>{
+  useEffect(() => {
     getData();
-  },[])//empty Dependencies array only one fuction can run
+  }, []); //empty Dependencies array only one fuction can run
 
   const saveData = async (newExpData) => {
-    const resp = await fetch(`http://localhost:3002/saveExpense`,{
-      headers:{
+    const resp = await fetch(`http://localhost:3002/saveExpense`, {
+      headers: {
         "Content-Type": "application/json",
-        "Accept" : "application/json"
+        Accept: "application/json",
       },
-      method: 'POST',
-      body:JSON.stringify(newExpData)
-
+      method: "POST",
+      body: JSON.stringify(newExpData),
     });
     const respObj = await resp.json();
     console.log(respObj);
     getData();
-  }
+  };
 
+  //13-02-2022
+  //delete
 
+  const deleteRow = async (id) => {
+    const resp = await fetch(`http://localhost:3002/deleteExpense/${id}`, {
+      method: "DELETE",
+    });
+    const data = await resp.json();
+    console.log(data);
+    getData();
+  };
+
+  //edit
+  const updateRow = async (rowData) => {
+    
+    rowData.expName="Hello world";
+    const resp = await fetch(
+      `http://localhost:3002/editExpense/${rowData._id}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(rowData),
+      }
+    );
+    const data = await resp.json();
+    console.log(data);
+    getData();
+  };
 
   return (
-    
-  //   <div>
-  //  <h1>Expense Tracker App</h1>
-  //  <section>
-  //  <ExpenseForm />
-  //  <ExpenseView  />
-  //  </section>
-  //  </div>
-  <section>
-
-    <h1>Expense Tracker App</h1>
-    <ExpenseForm setExpDataFun ={setExpData} expenseData={expData} saveDataFn={saveData}></ExpenseForm>
-    <ExpenseView data={expData}></ExpenseView>
-  </section>
-    
-
+    //   <div>
+    //  <h1>Expense Tracker App</h1>
+    //  <section>
+    //  <ExpenseForm />
+    //  <ExpenseView  />
+    //  </section>
+    //  </div>
+    <section>
+      <h1>Expense Tracker App</h1>
+      <ExpenseForm
+        setExpDataFun={setExpData}
+        expenseData={expData}
+        saveDataFn={saveData}
+      ></ExpenseForm>
+      <ExpenseView data={expData} deleteExpFn={deleteRow} editExpFn={updateRow} ></ExpenseView>
+    </section>
   );
 }
 
--- src/ExpenseView.js
+++ src/ExpenseView.js
@@ -7,14 +7,16 @@ function ExpenseView(props){
             // react fragment <> </>
             <div class="list">
                 {props.data.map((d,i)=>(
-                <ul key ={i}>
+                <ul key ={d._id}>
                     {/* //Unique Id in Html <ul id=i> using React <ul key={i}. */}
                     {/* <li>Expense Name:  {props.data[0].expName}</li> */}
+                    {/* <li contentEditable>Expense Name:  {d.expName}</li> */}
                     <li>Expense Name:  {d.expName}</li>
                     <li>Expense Amount:  {d.expAmt}</li>
                     <li>Expense Date: {d.expDate}</li>
-                    {/* <li>Expense Date: {props.data[0].expDate}</li>
-                    <center><button type="reset"> Reset </button></center> */}
+                    {/* <li>Expense Date: {props.data[0].expDate}</li> */}
+                     <center><button onClick={()=> props.deleteExpFn(d._id)}> delete </button></center>
+                     <center><button onClick={()=> props.editExpFn(d)}> edit</button></center>
                 </ul>))}
             </div>
         ):(<h2> No Expenses Found</h2>)}
